787a6a80ed3ac68d13178c8ef05b7a2bb8fb7335
170043 <170043@LAP-IT-MARTTIN>
2018-07-23 19:21:11 +0700

    Pemisahan untuk Function

---
+++ CIS.SDPM/Function/GetAPIToken.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace CIS.SDPM.Function
+{
+    class GetAPIToken
+    {
+        public static string getToken()
+        {
+            string strValue = "";
+            string strBody = "";
+            string BaseUrl = Common.GetBaseUrl() + "login";
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(BaseUrl);
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                request.Headers.Add("api-key", Common.GetAPIKey());
+                strBody = "{\n\"email\":\"" + Common.GetEmail() + "\"\n,\"password\":\"" + Common.GetPassword() + "\"\n}";
+                request.ContentLength = strBody.Length;
+
+
+                using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
+                {
+                    writer.Write(strBody);
+                }
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    var temp = reader.ReadLine();
+                    var jobj = JObject.Parse(temp);
+                    strValue = jobj["api-token"].ToString();
+
+                    Console.WriteLine("TOKEN : {0}", strValue);
+
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+            return strValue;
+        }
+    }
+}
--- CIS.SDPM/Program.cs
+++ CIS.SDPM/Program.cs
@@ -19,50 +19,11 @@ namespace CIS.SDPM
                                        SecurityProtocolType.Tls12;
             /* END untuk menghindari error underlying connection was closed */
 
-            Console.WriteLine("TOKEN {0}", getToken());
+            Console.WriteLine("TOKEN {0}", Function.GetAPIToken.getToken());
             Console.ReadKey();
         }
 
 
-        private static string getToken()
-        {
-            string strValue = "";
-            string strBody = "";
-            string BaseUrl = Common.GetBaseUrl() + "login";
-            try
-            {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(BaseUrl);
-                request.Method = "POST";
-                request.ContentType = "application/json";
-                request.Headers.Add("api-key", Common.GetAPIKey());
-                strBody = "{\n\"email\":\"" + Common.GetEmail() + "\"\n,\"password\":\"" + Common.GetPassword() + "\"\n}";
-                request.ContentLength = strBody.Length;
-
-
-                using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
-                {
-                    writer.Write(strBody);
-                }
-
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                {
-                    var temp = reader.ReadLine();
-                    var jobj = JObject.Parse(temp);
-                    strValue = jobj["api-token"].ToString();
-
-                    Console.WriteLine("TOKEN : {0}", strValue);
-
-                }
-                Console.ReadLine();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.ReadLine();
-            }
-
-            return strValue;
-        }
+       
     }
 }
