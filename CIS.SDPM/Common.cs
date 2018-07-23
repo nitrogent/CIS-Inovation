e2dcf7adcae07fc55ec2d11e71f0595170be8c03
170043 <170043@LAP-IT-MARTTIN>
2018-07-23 18:10:42 +0700

    Create Awal

---
+++ CIS.SDPM/Common.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace CIS.SDPM
+{
+    class Common
+    {
+        public static string GetBaseUrl()
+        {
+            string value = "";
+            try
+            {
+                value = ConfigurationManager.AppSettings.Get("urlBase");
+            }
+            catch (Exception ex)
+            {
+                value = ex.Message;
+            }
+
+            return value;
+        }
+
+        public static string GetEmail()
+        {
+            string value = "";
+            try
+            {
+                value = ConfigurationManager.AppSettings.Get("email");
+            }
+            catch (Exception ex)
+            {
+                value = ex.Message;
+            }
+
+            return value;
+        }
+
+        public static string GetPassword()
+        {
+            string value = "";
+            try
+            {
+                value = ConfigurationManager.AppSettings.Get("password");
+            }
+            catch (Exception ex)
+            {
+                value = ex.Message;
+            }
+
+            return value;
+        }
+
+        public static string GetAPIKey()
+        {
+            string value = "";
+            try
+            {
+                value = ConfigurationManager.AppSettings.Get("apiKey");
+            }
+            catch (Exception ex)
+            {
+                value = ex.Message;
+            }
+
+            return value;
+        }
+
+    }
+}
