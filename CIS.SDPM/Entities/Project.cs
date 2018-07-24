54906c4f50ca18598eb765a309c9d284af271c4c
nitrogent <marttin.liu@gmail.com>
2018-07-24 16:21:15 +0700

    Perubahan .NETFramework dan pembuatan class projects

---
+++ CIS.SDPM/Entities/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS.SDPM.Entities
+{
+    class Project
+    {
+        public int id { get; set; }
+        public string projectTitle { get; set; }
+        public DateTime createdOn { get; set; }
+        public string createdBy { get; set; } //FirstName + LastName
+    }
+}
